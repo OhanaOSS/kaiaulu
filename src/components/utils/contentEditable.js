93153a8f1fc6b70e85c00d7b0cecad1f45cb230e
Lassiter Gregg <lassitergregg@gmail.com>
2019-03-06 16:30:35 -0600

    added contentEditable to allow inline edits

---
+++ src/components/utils/contentEditable.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+export default function contentEditable(WrappedComponent) {
+
+  return class extends React.Component {
+
+    state = {
+      editing: false
+    }
+
+    toggleEdit = (e) => {
+      console.log(this.props)
+      e.stopPropagation();
+      if (this.state.editing) {
+        this.cancel();
+      } else {
+        this.edit();
+      }
+    };
+
+    edit = () => {
+      this.setState({
+        editing: true
+      }, () => {
+        this.domElm.focus();
+      });
+    };
+
+    save = () => {
+      this.setState({
+        editing: false
+      }, () => {
+        if (this.isValueChanged()) {
+          this.props.onSave({value: this.domElm.textContent})
+          console.log('Value is changed', this.domElm.textContent);
+        }
+      });
+    };
+
+    cancel = () => {
+      this.setState({
+        editing: false
+      });
+    };
+
+    isValueChanged = () => {
+      return this.props.value !== this.domElm.textContent
+    };
+
+    handleKeyDown = (e) => {
+      const { key } = e;
+      switch (key) {
+        case 'Enter':
+        case 'Escape':
+          this.save();
+          break;
+      }
+    };
+
+    render() {
+      let editOnClick = true;
+      const {editing} = this.state;
+      if (this.props.editOnClick !== undefined) {
+        editOnClick = this.props.editOnClick;
+      }
+      return (
+        <WrappedComponent
+          className={editing ? 'editing' : ''}
+          onClick={editOnClick ? this.toggleEdit : undefined}
+          contentEditable={editing}
+          ref={(domNode) => {
+            this.domElm = domNode;
+          }}
+          onBlur={this.save}
+          onKeyDown={this.handleKeyDown}
+          {...this.props}
+      >
+        {this.props.value}
+      </WrappedComponent>
+      )
+    }
+  }
+}
