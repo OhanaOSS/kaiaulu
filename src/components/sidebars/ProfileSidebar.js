5352c650baee4683d5728fd1a668a96235c0706d
Lassiter Gregg <lassitergregg@gmail.com>
2019-03-16 17:09:14 -0500

    fixed surname

--- src/components/sidebars/ProfileSidebar.js
+++ src/components/sidebars/ProfileSidebar.js
@@ -17,7 +17,7 @@ class ProfileSidebar extends Component {
             <Card>
                 <Card.Img variant="top" src="holder.js/100px180" />
                 <Card.Body>
-                    <Card.Title>{`${currentUser.name} ${currentUser}`}</Card.Title>
+                    <Card.Title>{`${currentUser.name} ${currentUser.surname}`}</Card.Title>
                     <Card.Text>
                     Some quick example text to build on the card title and make up the bulk of
                     the card's content.
